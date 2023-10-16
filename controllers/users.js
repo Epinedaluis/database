fa88a0fd27e1effba5a14dab47d6871969b26156
Eduardo Pineda Luis <isc20350307@gmail.com>
2023-10-16 10:24:19 -0600

    user creation test

--- controllers/users.js
+++ controllers/users.js
@@ -65,17 +65,57 @@ const pool = require('../db');
  }
 
 const addUser = async (req = request, res= response) => {
+
+    const {
+        username,
+        password,
+        email,
+        name,
+        lastname,
+        phonenumber = '',
+        role_id,
+        is_active = 1
+    } = req.body;
+
+    if(!username || !password || !email || !name || !lastname || !role_id){
+        res.status(400).json({msg: 'Missing information'});
+        return;
+    }
+
+    const user = [username, password, email, name, lastname, phonenumber, role_id, is_active]
+
     let conn;
 
     try{
         conn = await pool.getConnection();
 
-        const userAdded = await conn.query(usersModel.addRow, [user], (err) => {
+        const [usernameExists] = await conn.query(usersModel.getByUsername, [username],(err) =>{
             if (err) throw err;
         })
 
-        console.log(userAdded);
-        res.json(userAdded);
+        if(usernameExists){
+            res.status(409).json({msg: `Username ${username} already exists`});
+            return;
+        }
+
+        const [EmailExists] = await conn.query (usersModel.getByEmail, [email], (err) => {
+            if (err) throw err;
+        })
+
+        if(EmailExists){
+            res.status(409).json({msg: `Email ${email}  already exists`});
+            return;
+        }
+
+        const userAdded = await conn.query(usersModel.addRow, [...user], (err) => {
+            if (err) throw err;
+        })
+
+        if (userAdded.affectedRows == 0){
+            throw new Error('User not added')
+        }
+        
+        res.json({msg: 'User added succesfully'});
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
@@ -89,3 +129,6 @@ const addUser = async (req = request, res= response) => {
 module.exports = {listUsers, listUserByID, addUser}
 
 //Solo para creacion de endpoint --- routes    ---   Controllers  ---  Models (DB)
+// PARAMS LINK CON VALORES
+//QUERI SIGNOS NOMBRE Y VALOR EN LINK
+// BODY MANEJAR INFORMACION SENSIBLE
--- models/users.js
+++ models/users.js
@@ -14,6 +14,47 @@ const usersModel = {
     WHERE
     id = ?
     `,
+    getByUsername: `
+    SELECT
+    *
+    FROM
+    Users
+    WHERE
+    username= ?
+    `,
+    getByEmail: `
+    SELECT
+    *
+    FROM
+    Users
+    WHERE
+    email = ?
+    `,
+
+    addRow: `
+    INSERT INTO
+    Users(
+      username,
+      password,
+      email,
+      name,
+      lastname,
+      phonenumber,
+      role_id,
+      is_active
+    ) VALUES(
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?
+    )
+    `,
 };
+
+
 
 module.exports = usersModel;
